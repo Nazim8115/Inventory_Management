a3fc7dd360a8209c927a222ed7a53e0a03954ae0
Nazim8115 <nazim.nn459@gmail.com>
2023-09-18 23:55:57 +0530

    add validation

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -13,10 +13,28 @@ export default class ProductController {
   }
 
   getAddForm(req, res) {
-    res.render("new-product");
+    res.render("new-product", { errorMessage: null });
   }
 
   addNewProduct(req, res) {
+    const { name, price, imageUrl } = req.body;
+    let errors = [];
+    if (!name || name.trim() == "") {
+      errors.push("Name is required");
+    }
+    if (!price || parseFloat(price) < 1) {
+      errors.push("price must be a positive value");
+    }
+    try {
+      const validUrl = new URL(imageUrl);
+    } catch (err) {
+      errors.push("Url is invalid");
+    }
+
+    if (errors.length > 0) {
+      return res.render("new-product", { errorMessage: errors[0] });
+    }
+
     var data = req.body;
     ProductModel.add(data.name, data.price, data.desc, data.imageUrl);
     const result = ProductModel.get();
